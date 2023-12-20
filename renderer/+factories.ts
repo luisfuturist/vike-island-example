d8d3d8dc614e4b784d6bcf0bac960fed058a7e6a
Luis Float <me@luisfloat.com>
2023-12-20 18:08:04 -0300

    fix: pass factories as functions

--- renderer/+factories.ts
+++ renderer/+factories.ts
@@ -1,11 +1,7 @@
-import Clock from "../components/Clock.react?url";
-import Counter from "../components/Counter.react?url";
-import VGreenCounter from "../components/VGreenCounter.vue?url";
-import Section from "../components/Section.react?url";
 
 export default {
-  Counter,
-  Clock,
-  VGreenCounter,
-  Section,
+  Counter: () => import("~/components/Counter.react"),
+  Clock: () => import("~/components/Clock.react"),
+  VGreenCounter: () => import("~/components/VGreenCounter.vue"),
+  Section: () => import("~/components/Section.react"),
 };
